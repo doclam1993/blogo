adc2ba66a5fb2e22fd600d867d92d7f3001c55ad
doclam1993 <94809296+doclam1993@users.noreply.github.com>
2021-12-08 20:15:58 +0100

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+  /*
+   ** Runtime Config
+   */
+
+  publicRuntimeConfig: {
+    axios: {
+      baseURL: 'https://api.nuxtjs.dev'
+    }
+  },
+
+  /*
+   ** Modules - https://nuxtjs.org/docs/2.x/directory-structure/modules
+   */
+  modules: ['@nuxtjs/axios']
+}
